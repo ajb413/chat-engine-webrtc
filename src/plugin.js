34cfb3796db35ea04257933663e4effcbc33d0ca
Adam Bavosa <adamb@pubnub.com>
2018-08-22 16:58:27 -0700

    added stun server list

--- src/plugin.js
+++ src/plugin.js
@@ -2,6 +2,33 @@
  *
  */
 
+const rtcconfig = { iceServers : [{ "url" :
+    navigator.mozGetUserMedia    ? "stun:stun.services.mozilla.com" :
+    navigator.webkitGetUserMedia ? "stun:stun.l.google.com:19302"   :
+                                   "stun:23.21.150.121"
+}
+,   {url: "stun:stun.l.google.com:19302"}
+,   {url: "stun:stun1.l.google.com:19302"}
+,   {url: "stun:stun2.l.google.com:19302"}
+,   {url: "stun:stun3.l.google.com:19302"}
+,   {url: "stun:stun4.l.google.com:19302"}
+,   {url: "stun:23.21.150.121"}
+,   {url: "stun:stun01.sipphone.com"}
+,   {url: "stun:stun.ekiga.net"}
+,   {url: "stun:stun.fwdnet.net"}
+,   {url: "stun:stun.ideasip.com"}
+,   {url: "stun:stun.iptel.org"}
+,   {url: "stun:stun.rixtelecom.se"}
+,   {url: "stun:stun.schlund.de"}
+,   {url: "stun:stunserver.org"}
+,   {url: "stun:stun.softjoys.com"}
+,   {url: "stun:stun.voiparound.com"}
+,   {url: "stun:stun.voipbuster.com"}
+,   {url: "stun:stun.voipstunt.com"}
+,   {url: "stun:stun.voxgratia.org"}
+,   {url: "stun:stun.xten.com"}
+] };
+
 function uuid() {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000)
@@ -73,7 +100,7 @@ module.exports = (config) => {
             localStream = localStream || this.localStream;
             let callId = uuid();
             let localDescription; // WebRTC local description
-            let peerConnection = new RTCPeerConnection();
+            let peerConnection = new RTCPeerConnection(rtcconfig);
             this.callCache[callId] = peerConnection;
             peerConnection.oniceconnectionstatechange = () => {
                 if (peerConnection.iceConnectionState === 'disconnected') {
@@ -147,7 +174,7 @@ module.exports = (config) => {
                     }
 
                     let answerDescription;
-                    let peerConnection = new RTCPeerConnection();
+                    let peerConnection = new RTCPeerConnection(rtcconfig);
                     this.callCache[callId] = peerConnection;
 
                     // When ICE candidates become available, send them to the remote client
