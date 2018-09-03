bb78f95979e03496cb11ba146f81116586d9435f
Adam Bavosa <adamb@pubnub.com>
2018-09-03 13:44:40 -0700

    fixed 3rd call bug

--- example/app.js
+++ example/app.js
@@ -31,6 +31,21 @@ const submit = document.getElementById('submit');
 const hide = 'hide';
 const uuid = newUuid();
 
+const rtcConfig = {
+    iceServers: [{
+        'urls': [
+            'turn:w2.xirsys.com:80?transport=udp',
+            'turn:w2.xirsys.com:3478?transport=udp',
+            'turn:w2.xirsys.com:80?transport=tcp',
+            'turn:w2.xirsys.com:3478?transport=tcp',
+            'turns:w2.xirsys.com:443?transport=tcp',
+            'turns:w2.xirsys.com:5349?transport=tcp'
+        ],
+        'credential': '35426d02-a7c3-11e8-98a1-f9e0e877debe',
+        'username': '35426bfe-a7c3-11e8-a8bd-e7d0be3af999'
+    }]
+};
+
 let username;
 let localStream;
 
@@ -79,6 +94,7 @@ const ChatEngine = ChatEngineCore.create({
 function incomingCall(name) {
     return new Promise((resolve) => {
         acceptCallButton.onclick = function() {
+            ChatEngine.me.webRTC.disconnect();
             incomingCallModal.classList.add(hide);
             videoModal.classList.remove(hide);
             chatInterface.classList.add(hide);
@@ -98,6 +114,7 @@ function incomingCall(name) {
 function confirmCall(name) {
     return new Promise((resolve) => {
         yesCallButton.onclick = function() {
+            ChatEngine.me.webRTC.disconnect();
             callConfirmModal.classList.add(hide);
             resolve(true);
         }
@@ -224,6 +241,7 @@ ChatEngine.on('$.ready', (data) => {
     const onCallResponse = (acceptedCall) => {
         console.log('onCallResponse');
         if (acceptedCall) {
+
             videoModal.classList.remove(hide);
             chatInterface.classList.add(hide);
         }
