0bebf046abe0044f60eef367b3d8cf06ce6b0379
Adam Bavosa <adamb@pubnub.com>
2018-09-06 18:24:09 -0700

    turnify

--- example/app.js
+++ example/app.js
@@ -39,6 +39,12 @@ let username; // local user name
 let localStream; // Local audio and video stream
 let noVideoTimeout; // Used for checking if video connection succeeded
 
+// Xirsys API Info, not required for WebRTC, but it helps
+const turnApiUrl = 'https://pubsub.pubnub.com/v1/blocks/sub-key/sub-c-7c977f32-a1b3-11e8-bc5d-ae80c5ea0c92/turn-credentials';
+let turnToken;
+request(turnApiUrl, 'GET').then((response) => { turnToken = response });
+
+
 // Init the audio and video stream on this client
 getLocalStream().then((myStream) => {
     localStream = myStream;
@@ -179,8 +185,15 @@ ChatEngine.on('$.ready', (data) => {
 
             confirmCall(name).then((yesDoCall) => {
                 if (yesDoCall) {
-                    ChatEngine.me.webRTC.callUser(userToCall, {
-                        myStream: localStream
+                    // Get new TURN server credentials from Xirsys
+                    // Not Required but it is more reliable than peer-to-peer
+                    request(turnApiUrl, 'PUT', {
+                        'headers': { 'tok': turnToken }
+                    }).then((response) => {
+                        rtcConfig.iceServers = [response];
+                        ChatEngine.me.webRTC.callUser(userToCall, {
+                            myStream: localStream
+                        });
                     });
                 }
             });
@@ -384,4 +397,56 @@ function noVideo() {
     if (remoteVideo.paused) {
         alert(message);
     }
+}
+
+/**
+ * Helper function to make an HTTP request wrapped in an ES6 Promise.
+ *
+ * @param {String} url URL of the resource that is being requested.
+ * @param {String} method POST, GET, PUT, etc.
+ * @param {Object} options JSON Object with HTTP request options, "header"
+ *     Object of possible headers to set, and a body Object of a request body.
+ *
+ * @return {Promise} Resolves a parsed JSON Object or String response text if
+ *     the response code is in the 200 range. Rejects with response status text
+ *     when the response code is outside of the 200 range.
+ */
+function request(url, method, options) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        let contentTypeIsSet = false;
+        options = options || {};
+        xhr.open(method, url);
+
+        for (let header in options.headers) {
+            if ({}.hasOwnProperty.call(options.headers, header)) {
+                header = header.toLowerCase();
+                contentTypeIsSet = header === 'content-type' ? true : contentTypeIsSet;
+                xhr.setRequestHeader(header, options.headers[header]);
+            }
+        }
+
+        if (!contentTypeIsSet) {
+            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        }
+
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                let response;
+                try {
+                    response = JSON.parse(xhr.response);
+                } catch (e) {
+                    response = xhr.response;
+                }
+                resolve(response);
+            } else {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText,
+                });
+            }
+        };
+
+        xhr.send(JSON.stringify(options.body));
+    });
 }
